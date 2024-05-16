588a76a5c7c98fb7e5b3761758352b514282f636
nyitraibence <nyitrai100@gmail.com>
2024-05-16 07:40:20 +0100

    created a navbar

--- GameStore.Frontend/obj/Debug/net8.0/GameStore.Frontend.AssemblyInfo.cs
+++ GameStore.Frontend/obj/Debug/net8.0/GameStore.Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ec33ae586cd75e788afe87c0111c4ffa632c6a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66e569ca57ccf9273056bf87c8bf28865f7e6d8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
