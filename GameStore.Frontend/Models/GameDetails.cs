425fdf34b9004c4d85cfd0f9bd4dbc2c89951da4
nyitraibence <nyitrai100@gmail.com>
2024-05-17 08:40:09 +0100

    add editgame componenant

---
+++ GameStore.Frontend/Models/GameDetails.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace GameStore.Frontend.Models;
+
+public class GameDetails
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+
+    public string? GenreId{ get; set; }
+
+    public decimal Price { get; set; }
+
+    public DateOnly ReleaseDate { get; set; }
+}
