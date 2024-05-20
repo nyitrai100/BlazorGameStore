394ba61bc2767063ef5a10732d3958b447421add
nyitraibence <nyitrai100@gmail.com>
2024-05-20 11:09:45 +0100

    Created AddGame function to the Gamelist

--- GameStore.Frontend/obj/Debug/net8.0/GameStore.Frontend.AssemblyInfo.cs
+++ GameStore.Frontend/obj/Debug/net8.0/GameStore.Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f745841df678e58504e5517cf6b8527aa216aa0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7da097dc50ce024c65f92c9dba16eff0bcaf000")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
