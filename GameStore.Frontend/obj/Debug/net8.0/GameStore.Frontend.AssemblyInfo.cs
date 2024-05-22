418f15764168c69e34e4d1511e327ec26c7b5703
nyitraibence <nyitrai100@gmail.com>
2024-05-22 08:51:26 +0100

    created cancel button

--- GameStore.Frontend/obj/Debug/net8.0/GameStore.Frontend.AssemblyInfo.cs
+++ GameStore.Frontend/obj/Debug/net8.0/GameStore.Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+394ba61bc2767063ef5a10732d3958b447421add")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4eb6ad25f61608d28283a34f6e703cc8993dee4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
