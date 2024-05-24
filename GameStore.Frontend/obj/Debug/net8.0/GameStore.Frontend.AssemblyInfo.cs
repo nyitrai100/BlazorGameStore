d753a6c51a2ee3a666b44e5cad0e189f6dc1b096
nyitraibence <nyitrai100@gmail.com>
2024-05-24 08:00:22 +0100

    added validatiom message

--- GameStore.Frontend/obj/Debug/net8.0/GameStore.Frontend.AssemblyInfo.cs
+++ GameStore.Frontend/obj/Debug/net8.0/GameStore.Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c09e86a891ee85685e3bb2fbbea5376213e373cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62c34040697a4dc7a74b8897e7b04787474ec4c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
